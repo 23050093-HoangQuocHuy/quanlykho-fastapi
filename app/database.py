791235f2f7214e1c7eeeaea6f7fca1775c8cd79b
Huy Quoc <huy1995303@gmail.com>
2025-12-19 21:17:46 +0700

    Switch database from SQLite to PostgreSQL

--- app/database.py
+++ app/database.py
@@ -1,13 +1,23 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./InventoryManagement.db"
+# Lấy DATABASE_URL từ Environment Variable (Render / .env)
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}, echo=True, future=True)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    echo=True,
+    future=True
+)
 
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 def get_db():

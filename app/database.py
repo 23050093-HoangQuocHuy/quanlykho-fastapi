cf18e2d988bdc69891c49db09f170d0a84567209
Huy Quoc <huy1995303@gmail.com>
2025-12-19 22:09:23 +0700

    undo

--- app/database.py
+++ app/database.py
@@ -1,23 +1,13 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Lấy DATABASE_URL từ Environment Variable (Render / .env)
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = "sqlite:///./InventoryManagement.db"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    echo=True,
-    future=True
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}, echo=True, future=True)
 
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
 
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 def get_db():
